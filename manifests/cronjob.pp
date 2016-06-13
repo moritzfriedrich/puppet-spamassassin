d2fa8ed58af1a3291c65c67446ef1d633585556f
Moritz Friedrich <mf@cyon.ch>
2016-06-13 11:14:37 +0200

    add option to configure specific sa-update cronjobs

---
+++ manifests/cronjob.pp
@@ -0,0 +1,45 @@
+# Define: cronjob
+# Parameters:
+# [*gpghomedir*]
+# set the gpghomedir, default could be /var/lib/spamassassin/sa-update-keys
+# [*nogpg*]
+# enable or disable sa-update argument '--nogpg'
+# default: false
+# [*channel*]
+# set the channel domain
+# [*schedule*]
+# define the schedule of the cronjob, could be hourly, daily, weekly, monthly
+# default: daily
+#
+define spamassassin::cronjob (
+    $gpghomedir = undef,
+    $nogpg      = false,
+    $channel    = undef,
+    $schedule   = 'daily',
+    ) {
+
+    if $gpghomedir {
+        validate_absolute_path($gpghomedir)
+    }
+
+    validate_bool($nogpg)
+
+    if $channel {
+        validate_re($channel, '^(([a-zA-Z]{1})|([a-zA-Z]{1}[a-zA-Z]{1})|([a-zA-Z]{1}[0-9]{1})|([0-9]{1}[a-zA-Z]{1})|([a-zA-Z0-9][a-zA-Z0-9-_]{1,61}[a-zA-Z0-9]))\.([a-zA-Z]{2,6}|[a-zA-Z0-9-]{2,30}\.[a-zA-Z]{2,3})$', 'channel should be a valid domain')
+    }
+    
+    validate_re($schedule, '^(hourly|daily|weekly|monthly)$', 'cron parameter must have a value of: hourly, daily, weekly or monthly')
+
+    if !$channel and !$gpghomedir {
+        fail('Please define parameter gpghomedir or channel.')
+    }
+
+    file { "/etc/cron.${schedule}/spamassassin.${title}":
+        ensure  => file,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0755',
+        content => template("${module_name}/sa-update.cron.erb")
+    }
+
+}
